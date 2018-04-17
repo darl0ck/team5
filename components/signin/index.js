3171aeddcb0a2f414e3b25f7a6cc4fdfefdc872e
danmerus <leoghost@mail.ru>
2018-04-17 14:26:36 +0500

    lint

--- components/signin/index.js
+++ components/signin/index.js
@@ -1,16 +1,22 @@
 export default function SignIn({ signInString }) {
     return <div className="sign-in">
-        <img src={'https://imgur.com/u3c6Dsf'} alt="logo" /> /* почему не грузит? */
+        <img className="logo" src={'https://i.imgur.com/u3c6Dsf.png'} alt="logo" />
         <a className="sign-in__a" href="/auth">{ signInString }</a>
         <style jsx>{`
+            .logo
+            {
+                margin-bottom: -20px;
+            }
+
             .sign-in
             {
                 display: flex;
+                flex-direction: column;
                 justify-content: center;
                 align-items: center;
                 position: absolute;
-                height: 100%;
-                width: 100%;
+                height: 70%;
+                width: 97%;
             }
 
             .sign-in__a,
@@ -23,11 +29,17 @@ export default function SignIn({ signInString }) {
                 line-height: 40px;
                 padding: 20px 40px;
                 display: block;
-                border: 1px solid #bda095;
+                border: 2px solid #FF7F50;
                 text-align: center;
                 border-radius: 3px;
-                background-color: #FF7F50;
+                background-color: white;
                 color: #000;
+            }
+
+            .sign-in__a:hover
+            {
+                background-color: #f55649;
+                color: white;
             }
         `}</style>
     </div>;
